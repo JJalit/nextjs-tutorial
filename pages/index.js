c38a7d4aa3e19397f5013e08664e97cddf19a92a
jaeha <jaeha23@naver.com>
2022-01-11 11:20:13 +0900

    Styling Tips - Using classnames library to toggle classes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Alert from '../components/alert';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
@@ -10,6 +11,9 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, Welcome my World</p>
+        <Alert type="success">
+          <h2>경고 알림</h2>
+        </Alert>
         <p>
           (This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
